1e24f198c4ed981d59c70b3d168427d5ac472a95
Parry <pt987@nyu.edu>
2022-04-04 17:41:20 -0400

    finished

--- app.js
+++ app.js
@@ -6,6 +6,27 @@ const {
 } = require("./db");
 const path = require("path");
 
+const requireToken = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    const user = await User.byToken(token);
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+app.get("/api/auth", requireToken, (req, res, next) => {
+  requireToken(req, res, next);
+  res.send(req.user);
+});
+
+app.get("/api/auth/:id/notes", requireToken, (req, res, next) => {
+  requireToken(req, res, next);
+  res.send(req.user);
+});
+
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
 app.post("/api/auth", async (req, res, next) => {
@@ -16,20 +37,20 @@ app.post("/api/auth", async (req, res, next) => {
   }
 });
 
-app.get("/api/auth", async (req, res, next) => {
+app.get("/api/auth", requireToken, async (req, res, next) => {
   try {
-    res.send(await User.byToken(req.headers.authorization));
+    res.send(req);
   } catch (ex) {
     next(ex);
   }
 });
 
-app.get("/api/users/:id/notes", async (req, res, next) => {
+app.get("/api/users/:id/notes", requireToken, async (req, res, next) => {
   try {
     res.send(
       await Note.findAll({
         where: {
-          userId: req.params.id,
+          userId: req.user.id,
         },
       })
     );
